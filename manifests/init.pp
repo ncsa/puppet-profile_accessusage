505e1e055d51f379d7cd07abf342a45a98044367
David King <kingda@illinois.edu>
2022-09-15 15:48:45 -0500

    add user accessusage

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile_accessusage
+class profile_accessusage {
+  user { 'accessusage':
+    ensure  => 'present',
+    comment => 'Account for accessusage queries',
+    home    => '/'
+    shell   => '/sbin/nologin'
+    uid     => '989'
+    gid     => '983'
+  }
+}
